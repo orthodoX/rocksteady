cfbfb0d8738d4f043bc41ee77e004aa45d3b4375
Paul Mucur <paul@altmetric.com>
2018-11-08 15:15:02 +0000

    Support Docker tags with periods
    > A tag name must be valid ASCII and may contain lowercase and uppercase
    > letters, digits, underscores, periods and dashes. A tag name may not
    > start with a period or a dash and may contain a maximum of 128
    > characters.
    
    -- https://docs.docker.com/engine/reference/commandline/tag/#extended-description

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+DOCKER_TAG = /\w[\w.-]{,127}/.freeze
+
 Rails.application.routes.draw do
   root to: redirect('/apps')
 
@@ -13,7 +15,7 @@ Rails.application.routes.draw do
     get 'app/:id/status' => 'app#status', :as => :app_status
     get 'app/:id/nomad_status' => 'app#nomad_status', :as => :app_nomad_status
     get 'app/:id/images' => 'app#images', :as => :app_images
-    post 'app/:id/deploy/(:tag)' => 'app#deploy', :as => :app_deploy
+    post 'app/:id/deploy/(:tag)' => 'app#deploy', :as => :app_deploy, :tag => DOCKER_TAG
   end
 
   post '/webhook/:app' => 'webhook#handle'
