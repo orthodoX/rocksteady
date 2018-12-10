52afec307e46b6fffdd9b99860d7ad743603a00f
Paul Mucur <paul@altmetric.com>
2018-12-10 11:17:14 +0000

    Return all ECR images, not just the first page
    Unfortunately, ECR images do not have a guaranteed sort order and we've
    seen the latest image actually appear on the _second_ page of the
    DescribeImages response. For now, collect _all_ images for a repository
    and rely on limiting the number of available images at the ECR side.
    
    As we're now potentially loading huge numbers of images, use destructive
    array operations to convert them into JSON and sort them.

---
+++ spec/models/app_image_list_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe AppImageList do
+  describe '#as_json' do
+    it 'orders images with latest pushed first' do
+      stub_request(:post, 'https://ecr.eu-west-1.amazonaws.com/').to_return(body: <<~JSON)
+        {
+           "imageDetails": [
+              {
+                 "imageDigest": "decafbad",
+                 "imagePushedAt": 1,
+                 "imageSizeInBytes": 12,
+                 "imageTags": [ "oldest" ],
+                 "registryId": "test-job",
+                 "repositoryName": "test-job"
+              },
+              {
+                 "imageDigest": "decafbad",
+                 "imagePushedAt": 3,
+                 "imageSizeInBytes": 12,
+                 "imageTags": [ "latest" ],
+                 "registryId": "test-job",
+                 "repositoryName": "test-job"
+              },
+              {
+                 "imageDigest": "decafbad",
+                 "imagePushedAt": 2,
+                 "imageSizeInBytes": 12,
+                 "imageTags": [ "old" ],
+                 "registryId": "test-job",
+                 "repositoryName": "test-job"
+              }
+           ],
+           "nextToken": null
+        }
+      JSON
+      app = App.new(repository_name: 'test-job')
+      json = described_class.new(app).as_json
+
+      expect(json.first).to include(tags: ['latest'])
+    end
+  end
+end
